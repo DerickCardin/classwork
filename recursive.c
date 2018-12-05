0d3f2e3cb9b28ca1602e4993f89ea89105893605
Derick Cardin <derickc50@gmail.com>
2018-12-05 00:40:03 +0000

    First commit

---
+++ recursive.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int fact(int); // declares function to call later in int main (cannot run that function unless previously declared outside of int main)
+
+int main(void)
+{
+    //5!
+    int result = fact(5); // to redeclare as a long long it would be long long result = fact()
+    printf("5! = %i\n", result); // to redeclare as a long long %lld\n
+}
+
+int fact(int num) // to redeclare as long long it would be long long int fact(int num)
+{
+    if(num <= 1)
+    {
+        return 1;
+    }
+    return num * fact(num - 1);
+}
